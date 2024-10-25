96301b1e8b8d8c4e8505836fc7ae1764c32b66ec
ale66 <alessandro.provetti@gmail.com>
2024-10-25 16:29:07 +0200

    JS file for exercise

---
+++ 55-javascript/js/marks_converter.js
@@ -0,0 +1,24 @@
+
+/* Convert Italian exam marks into percentages */
+function pc2marks(marks) {
+
+    let converted = (marks / 100) * 30
+  
+    // Math.round() is a 'foreign' function that rounds up 50.65 --> 51 etc.
+    let pc = Math.round(converted);
+  
+    return pc;
+  } 
+  
+  
+// type this by yourself
+// let my_uk_exam_marks = [90, 68, 75, 82, 100]
+  
+  
+/* convert and show on the page the UK version of the marks */
+/*
+for (const m of my_uk_exam_marks){
+    let it_marks = pc2marks(m);
+    console.log(it_marks)
+  }
+/*
