0e5499505fc13626df5ad7ef30f31de2085b0199
ale66 <alessandro.provetti@gmail.com>
2024-11-14 23:28:22 +0000

    A/B lecture in

---
+++ 84-ab-testing/test-platform/scriptABtesting.js
@@ -0,0 +1,15 @@
+function randAssign() {
+  // Import pre-made functions in the Math library
+  // guess a random number between 0
+  // (inclusive),  and 1 (exclusive)
+  myrand = Math.random();
+
+  // half of the times (more or less) the 
+  // guessed number will be below 0.5:
+  if (myrand >= 0.5) {
+      return 'A'
+  } else {
+      return 'B'
+  }
+}
+`
