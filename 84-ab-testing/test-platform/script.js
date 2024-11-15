0b0d1773d815888d98634c5a2ba3c249b21bfc74
ale66 <alessandro.provetti@gmail.com>
2024-11-15 23:47:09 +0000

    A/B testing in

--- 84-ab-testing/test-platform/script.js
+++ 84-ab-testing/test-platform/script.js
@@ -4,12 +4,16 @@ function changeHeroTitle(choice) {
     // variable choice contains a string 'sent' 
     // by the invocation on line 30 or 32.
     if (choice == 'es') {
+
         // we change the HTML of myImage 
         // and re-assign the file where the image sits.
         document.getElementById('myHeroTitle').innerHTML = "Un banco de pruebas para funciones JS que modifican páginas"
+    
     } else {
+    
         // it must be Italian
         document.getElementById('myHeroTitle').innerHTML = "Un banco di prova per funzioni JS che modificano  le pagine"
+    
     }
 } // end of changeHeroTitle function
 
@@ -17,6 +21,7 @@ function changebg(choice) {
     /* Check out the details here:
     https://www.w3schools.com/jsref/prop_style_backgroundcolor.asp
     */
+    
     document.body.style.backgroundColor = choice
 
 } // end of changebg function
--- 84-ab-testing/test-platform/scriptABtesting.js
+++ 84-ab-testing/test-platform/scriptABtesting.js
@@ -2,14 +2,18 @@ function randAssign() {
   // Import pre-made functions in the Math library
   // guess a random number between 0
   // (inclusive),  and 1 (exclusive)
+  
   myrand = Math.random();
 
   // half of the times (more or less) the 
   // guessed number will be below 0.5:
   if (myrand >= 0.5) {
-      return 'A'
+  
+    return 'A'
+  
   } else {
-      return 'B'
+    
+    return 'B'
+  
   }
 }
-`
