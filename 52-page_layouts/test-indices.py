61cf1a9b5a011e4e05488c068894e196275a9df2
ale66 <alessandro.provetti@gmail.com>
2025-10-10 13:30:30 +0200

    Week 3 Recap

---
+++ 52-page_layouts/test-indices.py
@@ -0,0 +1,84 @@
+# type your name here when you land!
+# here I am, Fabio
+
+#Asya
+#Luca
+alist = [2, 3, 5, 7]
+
+# how long is the menu?
+n = len(alist)
+
+for index in range(n):
+    print(f'We are now working on index {index} and value {alist[index]}')
+    alist[index] = alist[index]*1.1
+    print(alist)
+
+'''
+# fix me!
+for price in alist:
+    price = price*1.1
+    print(f'The new price is {price}')
+'''
+
+print(alist)
+
+
+# In-class work
+
+'''
+# assigned to YRINA
+# Print a string vertically (one character per line) in reverse order
+mystring = 'Crystal Palace FC'
+'''
+
+'''
+# assigned to LUCA
+# print out the single string containing the names of each fruit without spaces
+fruitlist = ['apple', 'banana', 'cherry']
+# AP: fine but you're using an advance device already
+# LC: I try in an alternative way
+
+#fruitlist = ''.join(fruitlist)
+
+print(fruitlist)
+'''
+
+'''
+# assigned to ASYA
+# Alter lists: add newfruit to fruitlist only if not present already 
+newfruit = 'orange'
+if newfruit not in fruitlist
+    fruitlist.append(newfruit)
+print(fruitlist)
+'''
+# AP: good effort but consider the .append() method described yesterday
+# (the example with the cats...)
+
+teams = ['Chelsea FC', 'Arsenal FC', 'Crystal Palace FC', 'West Ham FC']
+
+howmany = len(teams)
+
+
+# assigned to GIULIA
+# print them as they are
+for single_team in teams:
+    # write here
+    print(teams)
+
+# assigned to FABIO
+# print w/o the ' FC' part!
+for single_team in teams:
+    # write here
+    len
+    
+
+# assigned to SARA
+# drop the FC part from the list permanently
+for i in range(howmany):
+    # write here
+    continue
+
+# assigned to FRANCESCA OR LORENZO
+# Challenge: remove duplicates from a list
+too_many = ['Chelsea FC', 'Arsenal FC', 'Crystal Palace FC', 'West Ham FC', 'Arsenal FC']
+
